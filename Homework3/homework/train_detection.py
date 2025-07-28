7bdb1c2b974d4cca4f1df4d0df11c25bdc31c438
Abe Mankavil <abe.m.mankavil@gmail.com>
2025-07-28 00:12:54 +0000

    Increase epochs for better metrics

--- Homework3/homework/train_detection.py
+++ Homework3/homework/train_detection.py
@@ -15,7 +15,7 @@ from .metrics import DetectionMetric
 def train(exp_dir: str = "logs",
           model_name: str = "detector",
           dataset_path: str = "drive_data",
-          num_epoch: int = 75,
+          num_epoch: int = 120,
           lr: float = 1e-3,
           batch_size: int = 64,
           weight_decay: float = 1e-4,
