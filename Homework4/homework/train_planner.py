5831c405e39dc44d81d96764cb41cd67b8478575
Abe Mankavil <abe.m.mankavil@gmail.com>
2025-08-04 17:16:46 -0400

    Begin HW4

---
+++ Homework4/homework/train_planner.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+import torch
+
+def train(exp_dir: str = "logs",
+          model_name: str = "",
+          dataset_path: str = "",
+          ):
+    pass
