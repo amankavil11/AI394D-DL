7600c1ca9a8b86f9d39eb44679832ef75a5c436b
Abe Mankavil <abe.m.mankavil@gmail.com>
2025-08-06 02:05:13 -0400

    L1Loss

--- Homework4/homework/train_planner.py
+++ Homework4/homework/train_planner.py
@@ -40,7 +40,7 @@ def train(exp_dir: str = "logs",
     train_data = load_data(f"{dataset_path}/train", batch_size=batch_size, shuffle=True, pin_memory=torch.cuda.is_available(), transform_pipeline=transform_pipeline)
     val_data = load_data(f"{dataset_path}/val", batch_size=batch_size, pin_memory=torch.cuda.is_available(), transform_pipeline=transform_pipeline)
 
-    loss_func = torch.nn.HuberLoss()
+    loss_func = torch.nn.L1Loss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
     global_step = 0
